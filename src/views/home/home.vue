d34f6455dce7c838f3313d87583d27d06e6ab3f9
mintsberry <mintberfly@gmail.com>
2020-02-26 11:50:58 +0800

    chore: 规范项目结构

---
+++ src/views/home/home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <nav-hader></nav-hader>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+        pathRewire: {
+          '/api': '  12'
+        }
+      }
+    }
+  }
+}
